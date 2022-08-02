2290f87d4aac948c14a701fb5a2b4ef88abe6ef7
Nil <nil@lin.am>
2022-08-02 14:10:46 +0800

    jest-mock-fetch: add username

--- packages/jest-mock-fetch/src/getUser.spec.ts
+++ packages/jest-mock-fetch/src/getUser.spec.ts
@@ -12,23 +12,23 @@ describe('getUser', () => {
 
   it('getUser 1', async () => {
     global.fetch = jest.fn(() => {
-      return Promise.resolve(new Response('{"id":1}'))
+      return Promise.resolve(new Response('{"id":1,"username":"alice"}'))
     })
 
     const user = await getUser(1)
     expect(global.fetch).toBeCalledTimes(1)
     expect(global.fetch).toBeCalledWith('https://dummyjson.com/users/1')
-    expect(user.id).toBe(1)
+    expect(user).toMatchObject({ id: 1, username: 'alice' })
   })
 
   it('getUser 2', async () => {
     global.fetch = jest.fn(() => {
-      return Promise.resolve(new Response('{"id":2}'))
+      return Promise.resolve(new Response('{"id":2,"username":"bob"}'))
     })
 
     const user = await getUser(2)
     expect(global.fetch).toBeCalledTimes(1)
     expect(global.fetch).toBeCalledWith('https://dummyjson.com/users/2')
-    expect(user.id).toBe(2)
+    expect(user).toMatchObject({ id: 2, username: 'bob' })
   })
 })
--- packages/jest-mock-fetch/src/getUser.ts
+++ packages/jest-mock-fetch/src/getUser.ts
@@ -1,9 +1,9 @@
 type User = {
   id: number
+  username: string
 }
 
 export async function getUser(id: number): Promise<User> {
   const res = await fetch(`https://dummyjson.com/users/${id}`)
-  const user = await res.json()
-  return { id: user.id }
+  return res.json()
 }
